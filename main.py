92e79e525e561c8904f4fd137e3f6215cfc9209f
Sergei Sachkov <sergey.sachkoff@gmail.com>
2019-09-18 23:28:19 -0400

    added link to registered users

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+users = []
+
 
 def verify_email(email):
     pattern = "[0-9A-Za-z]+@[a-z]+.com"
     result = re.match(pattern, email)
+    if result:
+        return True
+
+
+def verify_username(username):
+    pattern = "[0-9A-Za-z]+"
+    result = re.match(pattern, username)
     if result:
         return True
 
@@ -36,6 +45,8 @@ def index():
 
         if len(username) == 0:
             errorUser = "Username is required"
+        elif not verify_username(username):
+            errorUser = "You cannot use special symbols"
 
         if len(password) == 0:
             errorPass = "Password is required"
@@ -47,9 +58,16 @@ def index():
             errorEmail = "email is not correct"
 
         if not errorUser and not errorPass and not errorVerify and not errorEmail:
+            users.append(username)
             return redirect('/registered?username={username}'.format(username=username))
 
-    return render_template('edit.html', errorPass=errorPass, errorUser=errorUser, errorVerify=errorVerify, errorEmail=errorEmail)
+    return render_template('edit.html', errorPass=errorPass, errorUser=errorUser, errorVerify=errorVerify,
+                           errorEmail=errorEmail)
 
+
+@app.route("/whosregistered")
+def show_registered():
+
+    return render_template('whosregistered.html', users=users)
 
 app.run()
